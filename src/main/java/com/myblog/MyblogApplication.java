f1da3e994c96e16023e8c0fe5259144a6c7adaf2
xiaojun <xiaojun@DESKTOP-ERCUHFH>
2017-12-04 15:03:20 +0800

    添加页面布局

--- src/main/java/com/myblog/MyblogApplication.java
+++ src/main/java/com/myblog/MyblogApplication.java
@@ -1,16 +1,25 @@
 package com.myblog;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+import com.myblog.common.config.CommonPropertyConfiguration;
 
 /**
  *@author zyk
  */
 @SpringBootApplication
+@EnableConfigurationProperties(CommonPropertyConfiguration.class)
 public class MyblogApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MyblogApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MyblogApplication.class, args);
 	}
